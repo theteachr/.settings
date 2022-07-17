8a48cc8f3a76a64d279574d7d82fef0423d967b6
theteachr <sop.theteachr@gmail.com>
2022-07-17 23:26:28 +0530

    Update fish prompt and vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# opam configuration
+source /Users/theteachr/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
