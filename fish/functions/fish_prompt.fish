996b3a53d844bf884bec35e3ab4bfb2342ae438e
theteachr <sop.theteachr@gmail.com>
2022-07-17 23:25:22 +0530

    Update fish prompt: Sorin

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,12 @@
 function fish_prompt
-    if test $status -ne 0
-        set_color red
-    else
-        set_color brgreen
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
-    echo -n "><> "
-    set_color normal
-end
 
-function fish_right_prompt
-	set_color magenta
-    basename (prompt_pwd)
+    set_color -o
+    if fish_is_root_user
+        echo -n (set_color red)'# '
+    end
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
     set_color normal
 end
