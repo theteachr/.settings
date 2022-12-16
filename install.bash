78af9031755c2186443e10dea0f52f6001762089
theteachr <sop.theteachr@gmail.com>
2022-12-17 00:25:31 +0530

    Format

--- install.bash
+++ install.bash
@@ -3,7 +3,8 @@
 SETTINGS_DIR=$(pwd)
 DOT_CONFIG_DIR=${HOME}/.config
 
-ln -sf ${SETTINGS_DIR}/nvim ${DOT_CONFIG_DIR}/
-ln -sf ${SETTINGS_DIR}/.tmux.conf ${HOME}/
-ln -sf ${SETTINGS_DIR}/kitty ${DOT_CONFIG_DIR}/
-ln -sf ${SETTINGS_DIR}/bpython ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/nvim        ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/.tmux.conf  ${HOME}/
+ln -sf ${SETTINGS_DIR}/kitty       ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/bpython     ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/sway        ${DOT_CONFIG_DIR}/
