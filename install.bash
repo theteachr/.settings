408ac0d3d4aeda0d700c56b4c6a688d80a6f677b
theteachr <sop.theteachr@gmail.com>
2022-12-16 23:39:39 +0530

    Initial commit: script symlinking

---
+++ install.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+SETTINGS_DIR=$(pwd)
+DOT_CONFIG_DIR=${HOME}/.config
+
+ln -sf ${SETTINGS_DIR}/nvim ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/.tmux.conf ${HOME}/
+ln -sf ${SETTINGS_DIR}/kitty ${DOT_CONFIG_DIR}/
+ln -sf ${SETTINGS_DIR}/bpython ${DOT_CONFIG_DIR}/
