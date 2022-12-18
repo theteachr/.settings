d730720261248d037ea65ce17479a78dc2343026
theteachr <sop.theteachr@gmail.com>
2022-12-18 13:09:52 +0530

    Add tofi config

--- install.bash
+++ install.bash
@@ -10,18 +10,17 @@ AT_DOT_CONFIG=(
   sway
   hypr
   waybar
+  tofi
 )
 
 AT_HOME=(.tmux.conf)
 
 for config in "${AT_HOME[@]}"
 do
-  echo $config
   ln -sf ${SETTINGS_DIR}/$config ${HOME}/
 done
 
 for config_folder in "${AT_DOT_CONFIG[@]}"
 do
-  echo $config_folder
   ln -sf ${SETTINGS_DIR}/$config_folder ${DOT_CONFIG_DIR}/
 done
