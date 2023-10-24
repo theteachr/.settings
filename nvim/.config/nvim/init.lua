81e015210c1f81b98834e9277103edc2126b3815
theteachr <sop.theteachr@gmail.com>
2023-10-24 09:10:36 +0530

    :art: Non italic Kanagawa

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -340,6 +340,32 @@ require("catppuccin").setup({
     },
 })
 
+-- Default options:
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = false },
+    functionStyle = {},
+    keywordStyle = { italic = false },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+
 vim.cmd.colorscheme "kanagawa-dragon"
 
 -- }}}
